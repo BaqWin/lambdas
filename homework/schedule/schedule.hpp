50a285078d2084c5a5343c4fd264d1053fa184db
Marcin <marcinjagielak90@gmail.com>
2024-04-27 21:34:03 +0200

    Clang-format

--- homework/schedule/schedule.hpp
+++ homework/schedule/schedule.hpp
@@ -2,7 +2,7 @@
 #include <functional>
 #include <thread>
 
-template<typename Func, typename Sec, typename... Data>
+template <typename Func, typename Sec, typename... Data>
 void schedule(Func func, Sec sec, Data... d) {
     std::this_thread::sleep_for(sec);
     func(std::forward<Data>(d)...);
